340cca386e7e0697c8cac23d2f17890541cf7300
Misha Vakulich <m.vakulich@softteco.com>
2016-03-17 14:01:09 +0300

    add delete, various fixes of logic usage

--- app/src/main/java/com/kinvey/bookshelf/Book.java
+++ app/src/main/java/com/kinvey/bookshelf/Book.java
@@ -4,11 +4,14 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyDeleteCallback;
 import com.kinvey.java.core.KinveyClientCallback;
 import com.kinvey.java.dto.User;
 
@@ -19,6 +22,8 @@ public class Book extends AppCompatActivity implements View.OnClickListener {
     Client client;
 
     EditText name;
+
+    BookDTO book = new BookDTO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,44 @@ public class Book extends AppCompatActivity implements View.OnClickListener {
         findViewById(R.id.save2).setOnClickListener(this);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (book.containsKey("_id")) {
+            getMenuInflater().inflate(R.menu.menu_book, menu);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String id = getIntent().getStringExtra("id");
+
+        final ProgressDialog pd = new ProgressDialog(this);
+        if (id != null){
+            pd.show();
+            client.dataStore(BookDTO.COLLECTION, BookDTO.class).find(id, new KinveyClientCallback<BookDTO>() {
+                @Override
+                public void onSuccess(BookDTO bookDTO) {
+                    book = bookDTO;
+                    name.setText(book.getName());
+                    invalidateOptionsMenu();
+                    pd.dismiss();
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    pd.dismiss();
+                }
+            });
+        }
+
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -44,8 +87,8 @@ public class Book extends AppCompatActivity implements View.OnClickListener {
 
                     final ProgressDialog pd = new ProgressDialog(this);
                     pd.setMessage("Saving");
-
-                    client.dataStore(BookDTO.COLLECTION, BookDTO.class).save(new BookDTO(name.getText().toString()),
+                    book.setName(name.getText().toString());
+                    client.dataStore(BookDTO.COLLECTION, BookDTO.class).save(book,
                             new KinveyClientCallback<BookDTO>() {
                                 @Override
                                 public void onSuccess(BookDTO result) {
@@ -62,5 +105,34 @@ public class Book extends AppCompatActivity implements View.OnClickListener {
                 }
                 break;
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        final ProgressDialog pd = new ProgressDialog(this);
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_delete) {
+            pd.show();
+            client.dataStore(BookDTO.COLLECTION, BookDTO.class).delete(book.get("_id").toString(), new KinveyDeleteCallback() {
+                @Override
+                public void onSuccess(Integer integer) {
+                    pd.dismiss();
+                    finish();
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    pd.dismiss();
+                    Toast.makeText(Book.this, "Failed to delete", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        return true;
     }
 }
